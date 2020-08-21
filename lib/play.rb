243fc3ccdb986c4c8f33eaf77b98369efd1b60db
Kevin Glasgow <kglas001@gmail.com>
2020-08-21 22:07:56 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -35,17 +35,6 @@ def turn(board)
     turn(board)
   end
 end
-def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board, index)
-    move(board, index)
-    display_board(board)
-  else
-    turn(board)
-  end
-end
 
 # Define your play method below
 def play(board)
